1ab0072e5b392d13e698127711c5ea33b982b4d4
Vitalii Korol <vitaliikorol@192-168-0-101.rdsnet.ro>
2020-04-28 16:09:07 +0300

    Updates

---
+++ precache-manifest.27f22d73834f991d587fe16e5d3eda81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda922b339ed7e11256ca5df6119e154",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b88fccc24c878b502049",
+    "url": "./static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "7b8eb21849ad3896af6f",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "b88fccc24c878b502049",
+    "url": "./static/js/2.749da832.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.749da832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b8eb21849ad3896af6f",
+    "url": "./static/js/main.e2d143a1.chunk.js"
+  },
+  {
+    "revision": "1957fe11dd2dd30cfae0",
+    "url": "./static/js/runtime-main.701f4094.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.27f22d73834f991d587fe16e5d3eda81.js"
 );
 
 self.addEventListener('message', (event) => {
